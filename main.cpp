7cbfea9c72cc08d2befe3c99ddb9bc8db75b3330
Eric Wu <eric.dianhao.wu@gmail.com>
2022-04-02 16:54:47 -0700

    made a function to find forced solutions

--- main.cpp
+++ main.cpp
@@ -32,6 +32,49 @@ void printNumbers(int nums[9][9])
     printRow(nums[7]);
     printRow(nums[8]);
 }
+int findUniqueSolution(int r, int c, int nums[9][9])
+{ // this method returns the unique solution to a certain position, 0 if there is no unique solution, and -1 if a contradiction is found (no solution at all)
+
+    if (nums[r][c] != 0){
+        return nums[r][c];
+    }
+
+    bool poss[10] = {true, true, true, true, true, true, true, true, true, true}; // poss[i] will indicate whether i is a possible value at numbers[r][c]
+
+    for (int i = 0; i < 9; i++)
+    {
+        poss[nums[r][i]] = false;
+        poss[nums[i][c]] = false;
+    }
+
+    int gridRow = r / 3; // this will be one of 0,1,2
+    int gridCol = c / 3; // this will be one of 0,1,2
+    int helper[3][3] = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            int num = nums[helper[gridRow][i]][helper[gridCol][j]];
+            poss[num] = false;
+        }
+    }
+
+    int numberOfSolns = 0;
+    int lastSolution = -1;
+    for (int i = 1; i <= 9; i++)
+    {
+        if (poss[i])
+        {
+            lastSolution = i;
+            numberOfSolns++;
+        }
+    }
+    if (numberOfSolns > 1)
+    {
+        return 0;
+    }
+    return lastSolution;
+}
 
 int main()
 {
@@ -66,8 +109,16 @@ int main()
         for (int col = 0; col < 9; col++)
             if (numbers[row][col] == -16)
                 numbers[row][col] = 0;
+    // at this point we are done getting the numbers from the file into numbers.
 
-    printNumbers(numbers);
+    printNumbers(numbers); cout << "------" << endl;
+    
+    for (int i = 0; i < 9; i ++){
+        for (int j = 0; j < 9; j ++){
+            cout << findUniqueSolution(i,j,numbers);
+        }
+        cout << endl;
+    }
 
     return 0;
 }
