fb804f7c1d59bcfa03aea30c21f05f67ee802cb8
Eric Wu <eric.dianhao.wu@gmail.com>
2022-04-02 13:52:47 -0700

    reading from file now

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
-int main(){
+int ReadFromFile(string filename)
+{
+    string text;
+    fstream MyReadFile(filename);
+
+    int numbers[9][9];
+
+    while (getline(MyReadFile, text))
+    {
+        // Output the text from the file
+        if(text == "-----------"){}
+        cout << text << endl;
+    }
+    MyReadFile.close();
+    return 0;
+}
+
+int main()
+{
+    ReadFromFile("puzzle.txt");
+
     return 0;
 }
